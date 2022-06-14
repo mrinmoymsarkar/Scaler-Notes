c78d2c29542b260db915fa2ba83339cfa35d320e
mrinmoymsarkar <msarkar07@live.in>
2022-06-15 00:15:48 +0530

    Create Max subarray.js

---
+++ DSA/Day 11 Strings/Max subarray.js
@@ -0,0 +1,77 @@
+/*
+Q4. Maximum Subarray Easy
+Solved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA and get it resolved.
+Get help from TA.
+Problem Description
+You are given an integer array C of size A. Now you need to find a subarray (contiguous elements) so that the sum of contiguous elements is maximum.
+But the sum must not exceed B.
+
+
+Problem Constraints
+1 <= A <= 103
+1 <= B <= 109
+1 <= C[i] <= 106
+
+
+Input Format
+The first argument is the integer A.
+The second argument is the integer B.
+The third argument is the integer array C.
+
+
+Output Format
+Return a single integer which denotes the maximum sum.
+
+
+Example Input
+Input 1:
+A = 5
+B = 12
+C = [2, 1, 3, 4, 5]
+Input 2:
+
+A = 3
+B = 1
+C = [2, 2, 2]
+
+
+Example Output
+Output 1:
+12
+Output 2:
+
+0
+
+
+Example Explanation
+Explanation 1:
+We can select {3,4,5} which sums up to 12 which is the maximum possible sum.
+Explanation 2:
+
+All elements are greater than B, which means we cannot select any subarray.
+Hence, the answer is 0.
+*/
+module.exports = { 
+ //param A : integer
+ //param B : integer
+ //param C : array of integers
+ //return an integer
+	maxSubarray : function(A, B, C){
+        let max_sum = 0n
+        for (let i = 0; i<A ; i++){
+            let sum = 0n
+            for(let j = i; j< A; j++){
+                sum = sum + (C[j])
+                // console.log('sum', sum)
+                if( sum <= B){
+                    if(sum>max_sum) max_sum = sum
+                    // console.log('sum', sum)
+                }
+            }
+        }
+        return Number(max_sum);
+	}
+};
