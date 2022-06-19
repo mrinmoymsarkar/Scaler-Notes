24013dedc3d36b822bb79cf791004a37a0864bd8
mrinmoymsarkar <msarkar07@live.in>
2022-06-19 14:50:05 +0530

    Create 08 Rotate Matrix.js

---
+++ DSA/Day 12 - Arrays: 2D Matrices/08 Rotate Matrix.js
@@ -0,0 +1,91 @@
+/*
+ou are given a n x n 2D matrix A representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+You need to do this in place.
+
+Note: If you end up using an additional array, you will only receive partial score.
+
+
+
+Problem Constraints
+1 <= n <= 1000
+
+
+
+Input Format
+First argument is a 2D matrix A of integers
+
+
+
+Output Format
+Return the 2D rotated matrix.
+
+
+
+Example Input
+Input 1:
+
+ [
+    [1, 2],
+    [3, 4]
+ ]
+Input 2:
+
+ [
+    [1]
+ ]
+
+
+Example Output
+Output 1:
+
+ [
+    [3, 1],
+    [4, 2]
+ ]
+Output 2:
+
+ [
+    [1]
+ ]
+
+
+Example Explanation
+Explanation 1:
+
+ After rotating the matrix by 90 degree:
+ 1 goes to 2, 2 goes to 4
+ 4 goes to 3, 3 goes to 1
+Explanation 2:
+
+ 2D array remains the ssame as there is only element.
+
+*/
+
+module.exports = { 
+    //param A : array of array of integers 
+    //return nothing 
+    solve : function(A){ 
+       for(let i = 0; i<A.length; i++){
+           for(let j = i+1; j< A.length; j++){
+               let temp = A[i][j] 
+               A[i][j] = A[j][i]
+               A[j][i] = temp
+           }
+       }
+       //reverse row    
+       for(let i = 0; i<A.length; i++){
+           let y =0;
+           for(let j = A.length -1 ; j>= y; j--){
+               let temp = A[i][y] 
+               A[i][y] = A[i][j]
+               A[i][j] = temp
+               y++
+           }
+       }
+       return A
+    } 
+    
+};
