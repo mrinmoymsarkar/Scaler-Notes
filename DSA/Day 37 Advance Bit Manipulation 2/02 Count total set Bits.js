945bae730b4c7edb8c195ca78021cad190718f87
mrinmoymsarkar <msarkar07@live.in>
2022-07-19 01:35:12 +0530

    Create 02 Count total set Bits.js

---
+++ DSA/Day 37 Advance Bit Manipulation 2/02 Count total set Bits.js
@@ -0,0 +1,81 @@
+/*
+Problem Description
+
+Given a positive integer A, the task is to count the total number of set bits in the binary representation of all the numbers from 1 to A.
+
+Return the count modulo 109 + 7.
+
+
+
+Problem Constraints
+
+1 <= A <= 109
+
+
+
+Input Format
+
+First and only argument is an integer A.
+
+
+
+Output Format
+
+Return an integer denoting the ( Total number of set bits in the binary representation of all the numbers from 1 to A )modulo 109 + 7.
+
+
+
+Example Input
+
+Input 1:
+
+ A = 3
+Input 2:
+
+ A = 1
+
+
+Example Output
+
+Output 1:
+
+ 4
+Output 2:
+
+ 1
+
+
+Example Explanation
+
+Explanation 1:
+
+ DECIMAL    BINARY  SET BIT COUNT
+    1          01        1
+    2          10        1
+    3          11        2
+ 1 + 1 + 2 = 4 
+ Answer = 4 % 1000000007 = 4
+Explanation 2:
+
+ A = 1
+  DECIMAL    BINARY  SET BIT COUNT
+    1          01        1
+ Answer = 1 % 1000000007 = 1
+*/
+
+module.exports = { 
+ //param A : integer
+ //return an integer
+
+	solve : function solve(A){
+
+        if (A==0){
+            return 0;
+        } 
+        let k = Math.floor(Math.log2(A));
+        let ans = Math.pow(2,k-1) * k;
+        ans+= A - Math.pow(2,k) + 1;
+        ans+= solve(A - Math.pow(2,k));
+        return ans % (Math.pow(10,9)+7)
+    }	
+};
