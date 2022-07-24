d5c7b67c02e28153fe7d6b60e091aa7affda7276
mrinmoymsarkar <msarkar07@live.in>
2022-07-25 02:37:05 +0530

    Create hw 03 leap.js

---
+++ DSA/Day 17  Modular Arithmetic Introductio/hw 03 leap.js
@@ -0,0 +1,12 @@
+/*
+
+*/
+module.exports = { 
+ //param A : integer
+ //return an integer
+	solve : function(A){
+        if(A % 400 == 0) return 1
+        else if ((A % 4 == 0) &&( A %  100 != 0)) return 1
+        return 0
+	}
+};
